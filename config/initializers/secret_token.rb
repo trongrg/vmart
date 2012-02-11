d26a20f7e3e20e74d84e8d96e77184bbd36b3855
Trong Tran <trongrg@gmail.com>
2012-02-11 16:43:33 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vmart::Application.config.secret_token = 'bdb0f2a3e0669bde4f72c140c48489f4686c02196a9017b17b39b8b889148ac180d10a40ec83755f80660b720756718e07187fcb3b10b021037b7057378982f3'

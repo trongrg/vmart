6f2baad96d8cf8c853d81e89593af39dcacffa59
Trong Tran <trongrg@gmail.com>
2012-02-11 21:12:03 +0000

    change background

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Vmart
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.sass.load_paths ||= []
+    config.sass.load_paths << "#{Rails.root}/app/assets/stylesheets"
+    config.sass.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
   end
 end

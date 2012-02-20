b2fe5c2663ebce2ee31e3477b3dd73ea7d9cdb97
Trong Tran <trongrg@gmail.com>
2012-02-20 19:02:53 +0000

    add http basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :http_authenticate
+
+  protected
+  def http_authenticate
+    if Rails.env.downcase == "production"
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "john" && password == "vmartlogistics"
+      end
+    end
+  end
 end

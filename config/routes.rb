487015ffd5658204aa18ae0f049bb3ac22f59a74
Trong Tran <trongrg@gmail.com>
2012-02-20 20:56:05 +0000

    services, bank_details, consign_shipment

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,4 @@
 Vmart::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'homepage#index'
   get '/about' => 'homepage#about'
   get '/contact' => 'homepage#contact'
@@ -56,9 +7,6 @@ Vmart::Application.routes.draw do
   get '/documents' => 'homepage#documents'
   get '/tools' => 'homepage#tool'
   get '/brief_outlook' => 'homepage#brief_outlook'
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  get '/bank_details' => 'homepage#bank_details'
+  get '/consign_a_shipment' => 'homepage#consign_a_shipment'
 end
